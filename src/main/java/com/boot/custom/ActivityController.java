173b9d1d386ad661902fa9f5c2b7567ebf5abe84
hgarg <garg.hem@gmail.com>
2021-01-27 11:14:48 -0700

    Id changes

--- src/main/java/com/boot/custom/ActivityController.java
+++ src/main/java/com/boot/custom/ActivityController.java
@@ -12,26 +12,31 @@ public class ActivityController {
 
     @PostMapping("/save")
     public ResponseEntity<String> save (@RequestBody Map<String, Object> payload) {
+        System.out.println("in save"+payload.toString());
         return new ResponseEntity<>("Save", HttpStatus.OK);
     }
 
     @PostMapping("/publish")
     public ResponseEntity<String> publish (@RequestBody Map<String, Object> payload) {
+        System.out.println("in publish"+payload.toString());
         return new ResponseEntity<>("Publish", HttpStatus.OK);
     }
 
     @PostMapping("/validate")
     public ResponseEntity<String> validate (@RequestBody Map<String, Object> payload) {
+        System.out.println("in validate"+payload.toString());
         return new ResponseEntity<>("Validate", HttpStatus.OK);
     }
 
     @PostMapping("/stop")
     public ResponseEntity<String> stop (@RequestBody Map<String, Object> payload) {
+        System.out.println("in stop"+payload.toString());
         return new ResponseEntity<>("Stop", HttpStatus.OK);
     }
 
     @PostMapping("/execute")
     public ActivityResult execute (@RequestBody ExecutePayload payload) {
+        System.out.println("in execute"+payload.toString());
         for (Map<String, String> inArgument : payload.getInArguments()) {
             if(inArgument.containsKey("status") && inArgument.get("status").equals("true")){
                 return new ActivityResult("true");
